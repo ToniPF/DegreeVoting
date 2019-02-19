8a95e532a25157d0151181384903002cbdc592c5
Joan Palau <joanp4l4u@gmail.com>
2019-02-20 00:17:46 +0100

    afegit camp code (identifica l'assignatura de forma unica), str i repr

--- degree_voting/polls/polls_models/subject.py
+++ degree_voting/polls/polls_models/subject.py
@@ -3,6 +3,14 @@ from django.db import models
 
 class Subject(models.Model):
 
+    code = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=64)
     ects = models.IntegerField()
     description = models.TextField()
+    
+    def __str__(self):
+        return '{} {}'.format(str(self.code), str(self.title))
+    
+    def __repr__(self):
+        return '{id: {0}, title: {1}, ects: {2}, description: {3}}'\
+        .format(str(self.code), str(self.title), str(self.ects),\ str(self.description))
