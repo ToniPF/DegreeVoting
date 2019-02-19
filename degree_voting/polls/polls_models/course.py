14827b95b93eed9659ad61c0e64271fa9eb29f09
Joan Palau <joanp4l4u@gmail.com>
2019-02-20 00:30:39 +0100

    creada llista enumerada per course, afegit str i repr

--- degree_voting/polls/polls_models/course.py
+++ degree_voting/polls/polls_models/course.py
@@ -3,7 +3,27 @@ from . import degree, subject
 
 
 class Course(models.Model):
+    PRIMER = 1
+    SEGON = 2
+    TERCER = 3
+    QUART = 4
+    CINQUE = 5
+    COURSE_LIST = (
+        (PRIMER, 'Primer'),
+        (SEGON, 'Segon'),
+        (TERCER, 'Tercer'),
+        (QUART, 'Quart'),
+        (CINQUE, 'Cinquè'),
+    )
 
     degree_id = models.ForeignKey(degree.Degree, on_delete=models.CASCADE)
     subject_id = models.ForeignKey(subject.Subject, on_delete=models.CASCADE)
-    course = models.SmallIntegerField()
+    course = models.SmallIntegerField(choices=COURSE_LIST)
+    
+    def __str__(self):
+        return '{} {}'.format(str(self.degree_id), str(self.subject_id))
+    
+    def __repr__(self):
+        return '{id: {0}, degree_id: {1}, subject_id: {2}, course: {3}}'\
+        .format(str(self.pk), str(self.degree_id), str(self.subject_id),\
+        str(self.course))
