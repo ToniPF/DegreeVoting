6335986cf93c4c16ae32394683b294d9e0f41ca5
Joan Palau <joanp4l4u@gmail.com>
2019-02-20 00:09:11 +0100

    afegit str i repr

--- degree_voting/polls/polls_models/teacher.py
+++ degree_voting/polls/polls_models/teacher.py
@@ -6,3 +6,10 @@ class Teacher(models.Model):
     name = models.CharField(max_length=32)
     email = models.EmailField()
     image = models.FilePathField()
+    
+    def __str__(self):
+        return str(self.name)
+    
+    def __repr__(self):
+        return '{id: {0}, name: {1}, email: {2}, image: {3}}'\
+        .format(str(self.pk), str(self.name), str(self.email),\ str(self.image))
