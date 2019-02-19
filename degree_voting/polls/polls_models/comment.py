f36db1a1d5b6f1b81366ac8530ce5be00615fcdd
Joan Palau <joanp4l4u@gmail.com>
2019-02-20 00:03:45 +0100

    afegit str i repr

--- degree_voting/polls/polls_models/comment.py
+++ degree_voting/polls/polls_models/comment.py
@@ -4,3 +4,10 @@ from django.db import models
 class Comment(models.Model):
 
     comment = models.CharField(250)
+    
+    def __str__(self):
+        return self.comment[:25]
+    
+    def __repr__(self):
+        return '{id: {0}, comment: {1}}'.format(str(self.pk), str(self.comment))
+    
