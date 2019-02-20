ad179db21b75accc8c4755d33b261a096a435c4a
Toni <toni.pafe@gmail.com>
2019-02-20 20:21:46 +0100

    Added the links in the header

--- degree_voting/polls/urls.py
+++ degree_voting/polls/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='polls-home')
+    path('', views.home, name='polls-home'),
+    path('degrees/', views.degrees, name='polls-degrees'),
+    path('teachers-ranking/', views.teachers_ranking, name='teachers-ranking'),
+    path('subjects-ranking/', views.subjects_ranking, name='subjects-ranking'),
+
 ]
