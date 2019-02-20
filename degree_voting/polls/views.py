5e94085c22eb63f57de977784660e583e2f66b58
JoanPalau <joanp4l4u@gmail.com>
2019-02-20 21:14:05 +0100

    Implementada funció obtenir top 5 millors i pitjors

--- degree_voting/polls/views.py
+++ degree_voting/polls/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .polls_models.qualification import Qualification
+from .polls_models.assessment import Assessment
+
 
 
 def home(request):
@@ -20,18 +23,50 @@ def degrees(request):
 
 
 def teachers_ranking(request):
+
+    teachers = Qualification.objects.all()
+
+    worst_qualifies, best_qualifies = get_top_qualifying(5, teachers)
+
     context = {
         'title': 'Teachers Ranking',
         'header': 'polls/header.html',
         'footer': 'polls/footer.html',
+        'worst_qualifies': worst_qualifies,
+        'best_qualifies': best_qualifies,
     }
     return render(request, 'polls/ranking.html', context)
 
 
 def subjects_ranking(request):
+
+    subjects = Assessment.objects.all()
+
+    worst_qualifies, best_qualifies = get_top_qualifying(5, subjects)
+
     context = {
         'title': 'Subject Ranking',
         'header': 'polls/header.html',
         'footer': 'polls/footer.html',
+        'worst_qualifies': worst_qualifies,
+        'best_qualifies': best_qualifies,
     }
+
     return render(request, 'polls/ranking.html', context)
+
+
+def get_top_qualifying(maximum, listed):
+
+    worst_qualifies, best_qualifies = [], []
+
+    # Check for maximum qualified items
+    if listed:
+        length = len(listed)
+        if length >= maximum:
+            worst_qualifies = (reversed(listed.order_by('mark')[-maximum:]))
+            best_qualifies = listed.order_by('mark')[:maximum]
+        else:
+            worst_qualifies = (reversed(listed.order_by('mark')))
+            best_qualifies = (listed.order_by('mark'))
+
+    return worst_qualifies, best_qualifies
