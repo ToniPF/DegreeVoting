87b43cedd48c8d366bad6b311e4dedd89fc0dabd
Toni <toni.pafe@gmail.com>
2019-02-20 21:57:22 +0100

    Loading data at subjects template.

--- degree_voting/polls/polls_models/assessment.py
+++ degree_voting/polls/polls_models/assessment.py
@@ -7,12 +7,13 @@ class Assessment(models.Model):
     mark = models.FloatField()
     difficulty = models.FloatField()
     amount = models.IntegerField()
-    subject_id = models.ForeignKey(subject.Subject, on_delete=models.CASCADE)
+    subject = models.ForeignKey(subject.Subject, on_delete=models.CASCADE)
 
     def __str__(self):
         return 'Subject: {0}\t - Mark: {1}\t - Difficulty: {2}'.\
-            format(str(self.subject_id), str(self.mark), str(self.difficulty))
+            format(str(self.subject), str(self.mark), str(self.difficulty))
 
     def __repr__(self):
-        return '{Subject: {0}, Mark: {1}, Difficulty: {2} , Amount: {3}}'. \
-            format(str(self.subject_id), str(self.mark), str(self.difficulty), str(self.amount))
+        # return '{Subject: {0}, Mark: {1}, Difficulty: {2} , Amount: {3}}'. \
+        #    format(str(self.subject.code), str(self.mark), str(self.difficulty), str(self.amount))
+        return 'PATATA'
