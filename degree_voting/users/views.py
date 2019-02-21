a4548dc546fdf048f5f8e8870047ac70884fefa3
Toni <toni.pafe@gmail.com>
2019-02-21 19:45:27 +0100

    Created a UserRegisteredForm from UserCreationForm

--- degree_voting/users/views.py
+++ degree_voting/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 # message.debug
 # message.info
 # message.success
@@ -9,10 +9,10 @@ from django.contrib import messages
 
 
 def register(request):
-    form = UserCreationForm()
+    form = UserRegisterForm()
 
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
