36b50ed0a772d1aedf8938b3c8084013aec95cfc
Toni <toni.pafe@gmail.com>
2019-02-21 19:33:38 +0100

    Setup registration and alert message

--- degree_voting/users/views.py
+++ degree_voting/users/views.py
@@ -1,7 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+# message.debug
+# message.info
+# message.success
+# message.warning
+# message.error
 
 
 def register(request):
     form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form})
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('polls-home')
+
+    context = {
+        'title': 'Register',
+        'form': form
+    }
+    return render(request, 'users/register.html', {'context': context})
